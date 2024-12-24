2d25687f024b754923cb1c7de6cfa9b855f65df7
RonS <132860834+Ronitsabhaya75@users.noreply.github.com>
2024-12-24 13:51:24 -0600

    Delete src directory

--- src/components/auth/ProtectedRoute.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from 'react-router-dom';
-
-const ProtectedRoute = ({ element }) => {
-  const isAuthenticated = true; // Replace with actual authentication logic
-  return isAuthenticated ? element : <Navigate to="/login" />;
-};
-
-export default ProtectedRoute;
